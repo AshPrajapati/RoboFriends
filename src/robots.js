879d79fe649d42d47762ea87f9965fd62ee6cbc9
Aayush Prajapati <ashprajapati67@gmail.com>
2021-06-16 11:40:34 +0530

    adding to the git

---
+++ src/robots.js
@@ -0,0 +1,101 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    },
+    {
+        id:11,
+        name:'James Doe',
+        username: 'Roboshobo',
+        email:'Jame.Do@robo.com'
+    },
+    {
+        id:12,
+        name:'Andrea Roy',
+        username: 'Andraew Rosh',
+        email:'Andre.Rosh@james.com'
+    },
+    {
+        id:13,
+        name:'Aditya Verma',
+        username: 'Aditya Roshan',
+        email:'Adi.Rosh@yah.com'
+
+    },
+    {
+      id:14,
+      name:'Radewoosh Wu@',
+      username: 'Wu Rosh',
+      email:'WooshWu.Rosh@cool.com'
+
+    },
+    {
+        id:15,
+        name:'Rajesh Sharma✔',
+        username: 'Cool Creator',
+        email:'cool.sha@icyy.com'
+
+    },
+    {
+      id:16,
+      name:'Chellam',
+      username:'Specialist Chellam',
+      email:'chellam.uni@unknown.com'
+    }
+  ];
